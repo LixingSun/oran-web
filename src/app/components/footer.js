a05a727b99272ebc4946ce26bdabe77a10047d54
Lixing <slxtrjh325@gmail.com>
2024-05-15 14:16:55 +0800

    Init footer

---
+++ src/app/components/footer.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Footer() {
+  return (
+    <footer className="flex justify-center items-center px-32 py-10 bg-primary">
+      <div className="text-sm text-white">©2024 oran.zone</div>   
+    </footer>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Header from "./components/header" ;
+import Footer from "./components/footer";
 
 export default function Home() {
   return (
     <main className="min-h-screen pt-32">
       <Header />
       
+      <Footer />
     </main>
   );
 }
