817dac391ceb7183db9367e79dae9a9793f89267
Lixing <slxtrjh325@gmail.com>
2024-05-15 23:57:19 +0800

    Replace cookie server action with cookies-next

--- src/components/LangSwitcher.jsx
+++ src/components/LangSwitcher.jsx
@@ -1,11 +1,15 @@
 'use client';
 
-import { LOCALE_EN, LOCALE_CN } from "@/i18n/settings";
-import { switchLocaleAction } from "@/actions/switchLocale";
+import { LOCALE_EN, LOCALE_CN, LANGUAGE_COOKIE } from "@/i18n/settings";
+import { setCookie } from 'cookies-next';
+import { useRouter } from 'next/navigation';
 
 export default function LangSwitcher ({locale}) {
+  const router = useRouter();
+
   const handleClick = () => {
-    switchLocaleAction(locale == LOCALE_EN ? LOCALE_CN : LOCALE_EN);
+    setCookie(LANGUAGE_COOKIE, locale == LOCALE_EN ? LOCALE_CN : LOCALE_EN);
+    router.refresh();
   }
 
   return <button className="mx-4 text-white" onClick={handleClick}>{locale == LOCALE_EN ? "中文" : "EN"}</button> 
