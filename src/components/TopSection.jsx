41f22cd352f1ab296ebab9fef5b7f7fc5d1ccfe5
Lixing <slxtrjh325@gmail.com>
2024-06-21 22:54:46 +0800

    Provide CV in EN and ZH version

--- src/components/TopSection.jsx
+++ src/components/TopSection.jsx
@@ -4,20 +4,25 @@ import Image from "next/image";
 import { useTranslation } from 'react-i18next';
 import { CDN_HOST } from "@/config";
 import { NS_HOME } from "@/i18n.config";
+import { useMemo } from "react";
 
 export default function TopSection() {
-  const {t} = useTranslation([NS_HOME]);
+  const {t, i18n} = useTranslation([NS_HOME]);
+  const language = i18n.language;
+  const resumeLink = useMemo(() => {
+    return `${CDN_HOST}/home/Resume_LixingSun_${language.toUpperCase()}.pdf`;
+  }, [language]);
 
   return (
     <div className="flex justify-between p-6 bg-primary text-white flex-col lg:flex-row lg:px-32 lg:py-10">
       <div className="flex flex-col lg:w-1/2">
         <div className="text-3xl">{t("topSection.title")}</div>
         <div className="text-2xl mt-4">{t("topSection.subtitle")}</div>
-        <div className="text-base lg:text-lg mt-4 whitespace-pre-line">{t("topSection.paragraph")}</div>
+        <div className="text-base lg:text-lg mt-4 ">{t("topSection.paragraph")}</div>
         <div className="flex-1" />
         <div className="flex items-center mt-8">
           <div className="text-sm lg:text-base mr-4">{t("topSection.resume")}</div>
-          <a href={`${CDN_HOST}/home/Resume_LixingSun_EN.pdf`} target="_blank" download="cv">
+          <a href={resumeLink} target="_blank" download="cv">
             <Image
               src="/icons/download.svg"
               alt="Download CV"
