31207de7e8563117208db86bc8294d15c4bd5cd7
Lixing <slxtrjh325@gmail.com>
2024-06-26 22:35:51 +0800

    Add demo link to dota2 match visualizer

--- src/app/[locale]/demo/data.js
+++ src/app/[locale]/demo/data.js
@@ -3,6 +3,11 @@ const demoData = [
     key: "formBuilderDemo",
     url: "https://form-builder.oran.zone",
     thumbnail: "form-builder-demo/form_builder_demo_thumbnail.png"
+  },
+  {
+    key: "dota2MatchVisualzier",
+    url: "https://dota2.oran.zone",
+    thumbnail: "dota2-match-visualizer/dota2_match_visualizer_thumbnail.png"
   }
 ];
 
